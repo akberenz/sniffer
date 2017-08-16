9a2f5d4a2cd1b4360c9ae4ca545928294d5dc3ff
Brett Berenz <thebrettberenz@gmail.com>
2017-08-15 20:18:56 -0400

    Correct version in meta file

--- sniffer.meta.js
+++ sniffer.meta.js
@@ -5,7 +5,7 @@
 // @description  Sniff out hidden content on steamgifts.com posts
 // @icon         https://raw.githubusercontent.com/bberenz/sniffer/master/secret-agent.png
 // @include      *://*.steamgifts.com/*
-// @version      0.1.0
+// @version      1.0.1
 // @downloadURL  https://raw.githubusercontent.com/bberenz/sniffer/master/sniffer.user.js
 // @updateURL    https://raw.githubusercontent.com/bberenz/sniffer/master/sniffer.meta.js
 // @require      https://code.jquery.com/jquery-1.12.3.min.js
